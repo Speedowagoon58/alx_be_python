3f3fcdbf00747e7bce2dbd841e19c3e4e1688a8e
Speedowagoon58 <100315622+Speedowagoon58@users.noreply.github.com>
2024-12-16 00:22:50 +0100

    initial

---
+++ basic_operations.py
@@ -0,0 +1,14 @@
+number1 = 10
+number2 = 5
+
+# Addition
+addition_result = number1 + number2
+print(f"Addition of {number1} and {number2} is {addition_result}")
+
+# Subtraction
+subtraction_result = number1 - number2
+print(f"Subtraction of {number1} and {number2} is {subtraction_result}")
+
+# Multiplication
+multiplication_result = number1 * number2
+print(f"Multiplication of {number1} and {number2} is {multiplication_result}")
