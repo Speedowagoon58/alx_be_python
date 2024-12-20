db944c5b316c0345b708fa1fdb1d9ad325828247
Speedowagoon58 <100315622+Speedowagoon58@users.noreply.github.com>
2024-12-20 17:25:21 +0100

    multiplication script added

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,8 @@
+# User input
+number = float(input("Enter a number to see its multiplication table:"))
+
+# Generate and print the multiplication table
+for i in range(1, 11):  # Loop from 1 to 10
+    product = number * i
+    print(f"{number} * {i} = {product}")
+
