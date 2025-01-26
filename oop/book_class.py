975d91ce02eb4d59fce8752e3315c62aea4899ee
Speedowagoon58 <100315622+Speedowagoon58@users.noreply.github.com>
2025-01-26 22:36:01 +0100

    dsfsdf

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,13 +1,14 @@
-class Book: 
+class Book:
     def __init__(self, title, author, year):
-        self.title : "1984"
-        self.author : "George Orwell"
-        self.year : "1949"
+        self.title = title
+        self.author = author
+        self.year = year
+
     def __del__(self):
-        if self:
-            self.close()
-            print(f"Deleting {self.title}")
+        print(f"Deleting {self.title}")
+
     def __str__(self):
         return f"{self.title} by {self.author}, published in {self.year}"
+
     def __repr__(self):
-        f"Book('{self.title}', '{self.author}', {self.year})"
+        return f"Book('{self.title}', '{self.author}', {self.year})"
